561171e9d53d36ffa686f71acff29f1f34662e4e
Shreshth Tuli <shreshthtuli@gmail.com>
2018-09-28 14:51:07 +0530

    updted file names

--- compile.sh
+++ compile.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-g++ -O2 -march=native -o conv main11.cpp
+g++ -O2 -march=native -o conv main1.cpp
 g++ -O2 -march=native -o rev main2.cpp
 
--- main1.cpp
+++ main1.cpp
@@ -1,20 +1,20 @@
 #include <fstream>
 #include <sstream>
-#include "Graph.h"
 
 using namespace std;
 
+
 int main(int argc, char *argv[]){
     ifstream input(argv[1]);
-	ofstream ss;
-	ss.open(argv[2]);
+	ofstream output;
+    output.open(argv[2]);
 
     int nodes, edges, k, start, end;
-    int num_clauses = 0;
+    int num_clauses;
     // optimization : num_clauses can be calculated directly
 
     input >> nodes >> edges >> k;
-    // stringstream ss;
+    ostringstream ss;
 
     int **matrix = new int*[nodes];
     for(int i = 0; i < nodes; ++i)
@@ -26,94 +26,81 @@ int main(int argc, char *argv[]){
         matrix[end-1][start-1] = 1;
     }
 
-    for(int i = 0; i < nodes; i++){
-        for(int j = 0; j < nodes; j++){
-            cout << matrix[i][j] << " " ;
-        }
-        cout << endl;
-    }
+    int terms = k * (nodes *k + edges);
+    num_clauses = nodes + (3 * edges * k) + ((nodes*(nodes-1))/2 - edges) * k + k + edges + k * (k - 1) * (3 * nodes + 1); 
+    output << "p cnf " << terms << " " << num_clauses << endl;
 
+    int temp, a, b;
     int term = 1;
     // Every node belongs to atleast 1 group
     for(int i = 0; i < nodes; i++){
         for(int j = 0; j < k; j++){
             ss << term++ << " ";
         }
-        ss << 0 << endl;
-        num_clauses++;
+        ss << "0\n";
     }
 
     // ss << "Part 1 complete\n";
 
-    int bin;
+    temp = term;
     for(int i = 0; i < nodes; i++){
         for(int j = i+1; j < nodes; j ++){
+            a = 1 + k*i;
+            b = 1 + k*j;
             if(matrix[i][j] == 1){
                 // Neighbors should have atleast one common group
-                bin = 1;
-                while(bin < (1 << k) - 1){
-                    for(int x = 1; x <= k; x++){
-                        if((bin & (1<<(k-x))) == 0)
-                            ss << (x + k*i) << " ";
-                        else    
-                            ss << (x + k*j) << " ";
-                    }
-                    bin++;
-                    ss << 0 << endl;
-                    num_clauses++;
+                for(int x = 1; x <= k; x++){
+                    ss << -a << " " << -b << " " << term << " 0\n"
+                       << a << " " << -term << " 0\n"
+                       << b << " " << -term << " 0\n";
+                    term++; a++; b++; 
                 }
+                for(; temp < term; temp++){
+                    ss << temp << " ";
+                }
+                ss << "0\n";
             }
             else{
                 // Those that are not neighbors should not have any common group
                 for(int x = 1; x <= k; x++){
-                    ss << -(x + k*i) << " " << -(x + k*j) << " " << 0 << endl;
-                    num_clauses++;
+                    ss << -a << " " << -b << " 0\n";
+                    a++; b++; 
                 }
             }
         }
     }
-
-    // ss << "Part 2 complete\n";
 
     // Atleast 1 in each group
     for(int i = 1; i <= k; i++){
         for(int j = 0; j < nodes; j++){
             ss << k*j + i << " ";
         }
-        ss << 0 << endl;
-        num_clauses++;
+        ss << "0\n";
     }
 
-    // ss << "Part 3 complete\n";
-
     // Subgraph check
-    // Possible logic optimizations:
-    // 1. Use subgraph info to reduce num of check. Something complement of : a subset b and b subset c, means a subset c need not be checked
-    // 2. for a subset b and b subset a use a common check
     for(int i = 1; i < k+1; i++){
         for(int j = 1; j < k+1; j++){
             if(i == j)
                 continue;
-            bin = 1;
-            while(bin < (1 << nodes)){
-                for(int x = 0; x < nodes; x++){
-                    if((bin & (1<<(nodes-x-1))) == 0)
-                        ss << (i + k*x) << " ";
-                    else    
-                        ss << -(j + k*x) << " ";
-                }
-                bin++;
-                ss << 0 << endl;
-                num_clauses++;
+            a = i; b = j;
+            for(int x = 0; x < nodes; x++){
+                ss << -a << " " << b << " " << term << " 0\n" 
+                   << a << " " << -term << " 0\n" 
+                   << -b << " " << -term << " 0\n";
+                term++; 
+                a+=k; b+=k;
             }
+            for(; temp < term; temp++){
+                ss << temp << " ";
+            }
+            ss << "0\n";
+            // num_clauses++;
         }
     }
 
-    // ss << "Part 4 complete\n";
-
-    ss.seekp(ios_base::beg);
-    ss << "p cnf " << nodes*k << " " << num_clauses << endl;
-    ss.flush();
-    ss.close();
+    output << ss.str();
+    output.flush();
+    output.close();
     return 0;
 }
