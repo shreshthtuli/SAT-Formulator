a608ecf249fd31f7181fafac631272e05eccc4a9
shreshthtuli <34661516+shreshthtuli@users.noreply.github.com>
2018-09-29 20:35:23 +0530

    added python prob gen to script

--- test.bat
+++ test.bat
@@ -1,4 +1,5 @@
-testgen -N=200 -K=50 -F=test -Q=1 
+REM testgen -N=200 -K=50 -F=test -Q=1 
+python problemGenerator.py 5 2
 conv.exe test.graph test.satinput
 minisat test.satinput test.satoutput
 rev.exe test.satoutput test.graph test.subgraphs
